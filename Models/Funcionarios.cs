cfdff9ffa4e376481b795eee95b536c4baf07eae
1012168 <1012168@sal.ipg.pt>
2019-11-19 15:03:46 +0000

    Criado modelo dos funcionarios

---
+++ Models/Funcionarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETO_PNET.Models {
+    public class Funcionarios {
+
+        public int IdFuncionarios { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Numero { get; set; }
+    }
+}

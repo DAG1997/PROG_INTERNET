9bb46527c784a3654fb120382438b3e280bb1090
1012168 <1012168@sal.ipg.pt>
2019-11-26 15:49:45 +0000

    Modelo dos cargos e alteracoes no index

---
+++ Models/Cargos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETO_PNET.Models {
+    public class Cargos {
+        public int IdCargos { get; set; }
+
+        [Required]
+        public string NomeCargo { get; set; }
+
+        [Required]
+
+        public string Funcao  { get; set; }
+
+        
+    }
+}

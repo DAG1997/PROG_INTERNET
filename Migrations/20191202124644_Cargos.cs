1ecf1019557d3a444e50ad54fd4005022ff70912
1012168 <1012168@sal.ipg.pt>
2019-12-03 14:15:41 +0000

    migraçoes

---
+++ Migrations/20191202124644_Cargos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PROJETO_PNET.Migrations
+{
+    public partial class Cargos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

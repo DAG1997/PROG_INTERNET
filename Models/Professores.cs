db798c1e729c25467589788d892537ad1eaba098
Tiago Cunha <1700597@sal.ipg.pt>
2020-01-03 18:12:23 +0000

    validações professores

--- Models/Professores.cs
+++ Models/Professores.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,20 @@ namespace PROJETO_PNET.Models
 {
     public class Professores
     {
-        public int professoresId { get; set; }
+        [Required(ErrorMessage = "Please enter your ID")]
+        public int ProfessoresId { get; set; }
 
+        [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(50, MinimumLength = 3)]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Please enter your number")]
         public string Numero { get; set; }
 
+        [Required(ErrorMessage = "Please enter your classes")]
         public string Aulas { get; set; }
 
+        [Required(ErrorMessage = "Please enter your Email")]
         public string Email { get; set; }
         
   

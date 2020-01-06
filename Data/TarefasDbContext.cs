08b92b3d5119647b3a612b9c25836915d5460509
1012168 <1012168@sal.ipg.pt>
2020-01-06 12:33:34 +0000

    Color and delete professors controller/migration

--- Data/TarefasDbContext.cs
+++ Data/TarefasDbContext.cs
@@ -17,7 +17,7 @@ namespace PROJETO_PNET.Data {
 
             public DbSet<PROJETO_PNET.Models.Funcionario> Funcionarios { get; set; }
 
-          public DbSet<PROJETO_PNET.Models.Professores> Professores { get; set; }
+         
 
 
     }

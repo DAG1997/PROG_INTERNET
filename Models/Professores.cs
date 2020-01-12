1aac4baed88c5ea4a1eead57e957d80aaa191e2e
Tiago Cunha <1700597@sal.ipg.pt>
2020-01-12 19:07:32 +0000

    PROFESSORES MODEL

---
+++ Models/Professores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETO_PNET.Models
+{
+    public class Professores
+    {
+        [Required(ErrorMessage = "Please enter your ID")]
+        public int professoresId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Please enter your number")]
+        public string Numero { get; set; }
+
+        [Required(ErrorMessage = "Please enter your classes")]
+        public string Aulas { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+        public string Email { get; set; }
+
+    }
+}

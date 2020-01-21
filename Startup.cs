e2e415a8929ca5e451be6541c18474bb61a2bf22
1012168 <1012168@sal.ipg.pt>
2020-01-21 11:59:43 +0000

    login

--- Startup.cs
+++ Startup.cs
@@ -6,73 +6,117 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PROJETO_PNET.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PROJETO_PNET.Models;
 
-namespace PROJETO_PNET
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace PROJETO_PNET {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            //services.AddDefaultIdentity<IdentityUser>(
+            //    options => options.SignIn.RequireConfirmedAccount = false
+            //).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI();
+
+            services.Configure<IdentityOptions>(
+                options => {
+                    // Password settings
+                    options.Password.RequireDigit = true;
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequireLowercase = true;
+
+                    // Lockout
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.AllowedForNewUsers = true;
+
+                    // Users
+                    options.User.RequireUniqueEmail = true;
+
+                    // Sign in
+                    options.SignIn.RequireConfirmedAccount = false;
+                }
+            );
+
+            services.AddAuthorization(
+                options => {
+                    options.AddPolicy(
+                        "CanManageProfessores",
+                        policy => policy.RequireRole("manager", "admin")
+                    );
+
+                    // other policies ...
+                }
+            );
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
 
             services.AddDbContext<TarefasDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("ConnectionStringTarefas")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<TarefasDbContext>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                        options.UseSqlServer(Configuration.GetConnectionString("ConnectionStringTarefas")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(
+           IApplicationBuilder app, IWebHostEnvironment env,
+           TarefasDbContext db,
+           UserManager<IdentityUser> userManager,
+           RoleManager<IdentityRole> roleManager
+       ) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
-            }
-            else
-            {
+            } else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+
+            app.UseRouting();
 
             app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
+
+            SeedData.CreateRolesAsync(roleManager).Wait();
+
+            if (env.IsDevelopment()) {
+                SeedData.Populate(db);
+                SeedData.PopulateUsersAsync(userManager).Wait();
+            } else {
+                // Make sure that there is an admin account
+                // ...
+            }
         }
     }
 }

d608eda9406ea5b7d0a7993f7a228e83db40cce1
1012168 <1012168@sal.ipg.pt>
2020-01-22 13:26:21 +0000

    login v2

--- Startup.cs
+++ Startup.cs
@@ -87,7 +87,7 @@ namespace PROJETO_PNET {
        ) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                //app.UseDatabaseErrorPage();
             } else {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
